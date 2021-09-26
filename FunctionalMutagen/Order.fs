e9145214e6cca9b3ae4bd3a260cc9100dcd7dca3
newrirte <nirn24ru@gmail.com>
2021-09-26 23:14:29 +0700

    Load order from env and Cache

---
+++ FunctionalMutagen/Order.fs
@@ -0,0 +1,41 @@
+﻿namespace Mutagen.Func
+
+open Mutagen.Bethesda
+open Mutagen.Bethesda.Plugins.Order
+open Mutagen.Bethesda.Skyrim
+open Mutagen.Bethesda.Environments
+open System.Linq
+open System.Collections.Generic
+
+module private Internal =
+
+  let listingGetter (a: IEnumerable<IModListing<ISkyrimModGetter>>) =
+    a.Cast<IModListingGetter<ISkyrimModGetter>>()
+
+module LoadOrder =
+  
+  let ListedOrderFromEnv (skyrimRelease: SkyrimRelease) =
+    let env = GameEnvironment.Typical.Skyrim(skyrimRelease)
+    env.LoadOrder.ListedOrder
+
+  let PriorityOrderFromEnv (skyrimRelease: SkyrimRelease) =
+    let env = GameEnvironment.Typical.Skyrim(skyrimRelease)
+    env.LoadOrder.ListedOrder
+
+  let ListedOrderFromEnvRevers (skyrimRelease: SkyrimRelease) =
+    let env = GameEnvironment.Typical.Skyrim(skyrimRelease)
+    env.LoadOrder.ListedOrder
+    |>Seq.rev
+
+  let PriorityOrderFromEnvRevers (skyrimRelease: SkyrimRelease) =
+    let env = GameEnvironment.Typical.Skyrim(skyrimRelease)
+    env.LoadOrder.ListedOrder
+    |>Seq.rev
+
+module Cache =
+  
+  let ToImmutableLinkCache getter =
+    (Internal.listingGetter getter).ToImmutableLinkCache()
+
+  let ToUntypedImmutableLinkCache getter =
+    (Internal.listingGetter getter).ToUntypedImmutableLinkCache
