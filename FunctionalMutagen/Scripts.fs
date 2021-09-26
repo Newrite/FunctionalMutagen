46d2d003051c97a89c46928df081d52ee40e737d
newrirte <nirn24ru@gmail.com>
2021-09-26 22:43:26 +0700

    Base implementation module script

---
+++ FunctionalMutagen/Scripts.fs
@@ -0,0 +1,79 @@
+﻿namespace Mutagen.Func
+
+open Mutagen.Bethesda.Skyrim
+open Mutagen.Bethesda
+
+module Scripts =
+
+  [<RequireQualifiedAccess>]
+  module ScriptEntry =
+
+    ///<summary>Create new ScriptEntry with ScriptName and list of ScriptProperty, can fill with Scripts.ScriptProperty module.</summary>
+    let New entryName (propertyList: ScriptProperty list) =
+      let entry = new ScriptEntry(Name = entryName)
+      propertyList
+      |>List.iter (fun property -> entry.Properties.Add(property))
+      entry
+
+  [<RequireQualifiedAccess>]
+  module ScriptProperty =
+
+    ///Internal function, build ExtendedList from F# list, need for PropertyList functions
+    let private buildExtendList values =
+      let l = new Noggog.ExtendedList<_>()
+      values
+      |>List.iter (fun v -> l.Add(v))
+      l
+    
+    ///<summary>Create new ScriptFloatProperty, field name and float32 value.</summary>
+    let Float name value =
+      new ScriptFloatProperty(Name = name, Data = value)
+
+    ///<summary>Create new ScriptBoolProperty, field name and bool value.</summary>
+    let Bool name value =
+      new ScriptBoolProperty(Name = name, Data = value)
+
+    ///<summary>Create new ScriptStringProperty, field name and string value.</summary>
+    let String name value =
+      new ScriptStringProperty(Name = name, Data = value)
+
+    ///<summary>Create new ScriptIntProperty, field name and int value.</summary>
+    let Integer name value =
+      new ScriptIntProperty(Name = name, Data = value)
+
+    ///<summary>Create new ScriptObjectProperty, field name and Bethesda object, 
+    ///need link object so you can use FormKeys from Mutagen.Bethesda.FormKeys lib 
+    ///lib link: https://github.com/Mutagen-Modding/Mutagen.Bethesda.FormKeys.</summary>
+    let Object name (object: Plugins.FormLink<#ISkyrimMajorRecordGetter>) =  //Need cast and flex type for interop with C#
+      new ScriptObjectProperty(Name = name, Object = object.Cast<ISkyrimMajorRecordGetter>())
+
+    ///<summary>Create new ScriptFloatListProperty, field name and float32 list value.</summary>
+    let FloatList name values =
+      new ScriptFloatListProperty(Name = name, Data = buildExtendList values)
+
+    ///<summary>Create new ScriptBoolListProperty, field name and bool list value.</summary>
+    let BoolList name values =
+      new ScriptBoolListProperty(Name = name, Data = buildExtendList values)
+
+    ///<summary>Create new ScriptStringListProperty, field name and string list value.</summary>
+    let StringList name values =
+      new ScriptStringListProperty(Name = name, Data = buildExtendList values)
+
+    ///<summary>Create new ScriptIntListProperty, field name and int list value.</summary>
+    let IntegerList name values =
+      new ScriptIntListProperty(Name = name, Data = buildExtendList values)
+
+    ///<summary>Create new ScriptObjectListProperty, field name and Bethesda objects, 
+    ///for create list of objects use Scripts.ScriptProperty.Object.</summary>
+    let ObjectList name (values: ScriptObjectProperty list) =
+      new ScriptObjectListProperty(Name = name, Objects = buildExtendList values)
+
+  [<RequireQualifiedAccess>]
+  module VirtualMachineAdapter =
+    
+    ///<summary>Create new VirtualMachineAdapter, can fill ScriptEntry list with Scripts.ScriptEntry.New.</summary>
+    let New (scriptEntryList: ScriptEntry list) =
+      let vm = new VirtualMachineAdapter()
+      scriptEntryList
+      |>List.iter (fun e -> vm.Scripts.Add(e))
+      vm
---
+++ FunctionalMutagenTest/Program.fs
@@ -0,0 +1,62 @@
+﻿open Mutagen.Func
+open Mutagen.Bethesda
+open Mutagen.Bethesda.Plugins.Order.Internals
+open Mutagen.Bethesda.Plugins.Order
+open Mutagen.Bethesda.Plugins
+open Mutagen.Bethesda.Skyrim
+open Mutagen.Bethesda.FormKeys.SkyrimSE
+open Mutagen.Bethesda.Environments
+open System.Linq
+open System.Collections.Generic
+
+open Mutagen.Func.Scripts
+
+//let env = GameEnvironment.Typical.Skyrim(SkyrimRelease.SkyrimSE)
+
+let test (a: IEnumerable<IModListing<ISkyrimModGetter>>) =
+  let casted = a.Cast<IModListingGetter<ISkyrimModGetter>>()
+  let b = let x = casted.Weapon() in x.WinningOverrides()
+  b
+
+//test env.LoadOrder.PriorityOrder |> printfn "%A"
+
+let entry =
+  VirtualMachineAdapter.New [
+
+    ScriptEntry.New "XPScript" [
+      ScriptProperty.Float "AmountXP" 20.f
+      ScriptProperty.Bool "GiveExp" true
+      ScriptProperty.Integer "Int amount" 20
+      ScriptProperty.String "This is string" "Hello"
+      ScriptProperty.Object "Katana" Skyrim.Weapon.AkaviriKatana
+    ]
+
+    ScriptEntry.New "XPScriptList" [
+      ScriptProperty.FloatList "Name" [ 10.f; 20.f; 30.f ]
+      ScriptProperty.BoolList "BoolName" [ true; false; true; true ]
+      ScriptProperty.IntegerList "IntName" [ 10; 20; 30 ]
+      ScriptProperty.StringList "StrName" [ "Helo"; "World"; "" ]
+      ScriptProperty.ObjectList "ObjList" [
+        ScriptProperty.Object "Katana" Skyrim.Weapon.AkaviriKatana
+        ScriptProperty.Object "Armor" Skyrim.Armor.ArmorAstrid
+        ]
+
+    ]
+  ]
+
+entry.Scripts
+|>Seq.iter (fun s ->
+    Seq.iter (fun (p: ScriptProperty) -> 
+      match p with
+      | :? ScriptFloatProperty as property -> printfn "Float: %s %f %A" property.Name property.Data property.Flags
+      | :? ScriptBoolProperty as property -> printfn "Bool: %s %b %A" property.Name property.Data property.Flags
+      | :? ScriptIntProperty as property -> printfn "Int: %s %d %A" property.Name property.Data property.Flags
+      | :? ScriptStringProperty as property -> printfn "String: %s %s %A" property.Name property.Data property.Flags
+      | :? ScriptObjectProperty as property -> printfn "Object: %s %A %A" property.Name property.Object property.Flags
+      | :? ScriptFloatListProperty as property -> printfn "FloatList: %s %A %A" property.Name property.Data property.Flags
+      | :? ScriptBoolListProperty as property -> printfn "BoolList: %s %A %A" property.Name property.Data property.Flags
+      | :? ScriptStringListProperty as property -> printfn "StringList: %s %A %A" property.Name property.Data property.Flags
+      | :? ScriptIntListProperty as property -> printfn "IntList: %s %A %A" property.Name property.Data property.Flags
+      | :? ScriptObjectListProperty as property -> printfn "ObjectList: %s %A %A" property.Name property.Objects property.Flags
+      | _ -> ()) s.Properties
+    )
