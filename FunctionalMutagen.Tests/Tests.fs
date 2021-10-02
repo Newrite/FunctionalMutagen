954f7cb0596c59f9db4be84dd471cef3ce14dcfa
newrirte <nirn24ru@gmail.com>
2021-10-02 19:18:09 +0700

    Add script propertys tests, only single property

--- FunctionalMutagen.Tests/Tests.fs
+++ FunctionalMutagen.Tests/Tests.fs
@@ -1,10 +1,10 @@
 ﻿module Tests
 
 open Mutagen.Func
+open Mutagen.Bethesda.Skyrim
 open Mutagen.Bethesda.FormKeys.SkyrimSE
 open Xunit
 open Swensen.Unquote
-open System
 
 module ScriptsTest =
 
@@ -35,3 +35,69 @@ module ScriptsTest =
       ]
 
     test <@ (isNull >> not) vma @>
+
+  [<Fact>]
+  let ``Float script propertys from DSL must be equal float propertys from Mutagen`` ()=
+    
+    let floatDSLPropertyZero = Scripts.ScriptProperty.float "testZero" 0.f
+    let floatDSLPropertyPos = Scripts.ScriptProperty.float "testPos" 2000.f
+    let floatDSLPropertyNeg = Scripts.ScriptProperty.float "testNeg" -2000.f
+
+    let floatMutagenPropertyZero = new ScriptFloatProperty(Name = "testZero", Data = 0.f)
+    let floatMutagenPropertyPos = new ScriptFloatProperty(Name = "testPos", Data = 2000.f)
+    let floatMutagenPropertyNeg = new ScriptFloatProperty(Name = "testNeg", Data = -2000.f)
+    
+    test <@ floatDSLPropertyZero.Equals(floatMutagenPropertyZero) @>
+    test <@ floatDSLPropertyPos.Equals(floatMutagenPropertyPos) @>
+    test <@ floatDSLPropertyNeg.Equals(floatMutagenPropertyNeg) @>
+
+  [<Fact>]
+  let ``Integer script propertys from DSL must be equal integer propertys from Mutagen`` ()=
+    
+    let integerDSLPropertyZero = Scripts.ScriptProperty.integer "testZero" 0
+    let integerDSLPropertyPos = Scripts.ScriptProperty.integer "testPos" 2000
+    let integerDSLPropertyNeg = Scripts.ScriptProperty.integer "testNeg" -2000
+
+    let integerMutagenPropertyZero = new ScriptIntProperty(Name = "testZero", Data = 0)
+    let integerMutagenPropertyPos = new ScriptIntProperty(Name = "testPos", Data = 2000)
+    let integerMutagenPropertyNeg = new ScriptIntProperty(Name = "testNeg", Data = -2000)
+    
+    test <@ integerDSLPropertyZero.Equals(integerMutagenPropertyZero) @>
+    test <@ integerDSLPropertyPos.Equals(integerMutagenPropertyPos) @>
+    test <@ integerDSLPropertyNeg.Equals(integerMutagenPropertyNeg) @>
+
+  [<Fact>]
+  let ``String script propertys from DSL must be equal string propertys from Mutagen`` ()=
+    
+    let stringDSLPropertyWhitespaces = Scripts.ScriptProperty.string "testWhitespaces" "     "
+    let stringDSLPropertyVoid = Scripts.ScriptProperty.string "testVoid" ""
+    let stringDSLProperty = Scripts.ScriptProperty.string "testStr" "Hello Test"
+
+    let stringMutagenPropertyWhitespaces = new ScriptStringProperty(Name = "testWhitespaces", Data = "     ")
+    let stringMutagenPropertyVoid = new ScriptStringProperty(Name = "testVoid", Data = "")
+    let stringMutagenProperty = new ScriptStringProperty(Name = "testStr", Data = "Hello Test")
+    
+    test <@ stringDSLPropertyWhitespaces.Equals(stringMutagenPropertyWhitespaces) @>
+    test <@ stringDSLPropertyVoid.Equals(stringMutagenPropertyVoid) @>
+    test <@ stringDSLProperty.Equals(stringMutagenProperty) @>
+
+  [<Fact>]
+  let ``Bool script propertys from DSL must be equal bool propertys from Mutagen`` ()=
+    
+    let boolDSLPropertyTrue = Scripts.ScriptProperty.bool "testTrue" true
+    let boolDSLPropertyFalse = Scripts.ScriptProperty.bool "testFalse" false
+
+    let boolMutagenPropertyTrue = new ScriptBoolProperty(Name = "testTrue", Data = true)
+    let boolMutagenPropertyFalse = new ScriptBoolProperty(Name = "testFalse", Data = false)
+    
+    test <@ boolDSLPropertyTrue.Equals(boolMutagenPropertyTrue) @>
+    test <@ boolDSLPropertyFalse.Equals(boolMutagenPropertyFalse) @>
+
+  [<Fact>]
+  let ``Object script property from DSL must be equal object property from Mutagen`` ()=
+    
+    let objectDSLProperty = Scripts.ScriptProperty.object "testKatana" Skyrim.Weapon.AkaviriKatana
+
+    let objectMutagenProperty = new ScriptObjectProperty(Name = "testKatana", Object = Skyrim.Weapon.AkaviriKatana.Cast<ISkyrimMajorRecordGetter>())
+    
+    test <@ objectDSLProperty.Equals(objectMutagenProperty) @>
